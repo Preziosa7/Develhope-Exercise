5953513bdeb09ed052cf700554772c17894b2304
Preziosa7 <agbonkheseprecious@gmail.com>
2026-01-20 15:04:54 +0100

    local_storage.js

--- esercizio-classi/local_storage.js
+++ esercizio-classi/local_storage.js
@@ -8,3 +8,8 @@ function saveUser() {
   localStorage.setItem("user", JSON.stringify(user));
 }
  
+function getUser() {
+  const savedUser = localStorage.getItem("user");   // returns a string
+  const parsedUser = JSON.parse(savedUser);         // convert string → object
+  console.log(parsedUser);
+}
